f8bd6ee7c70af6a732f65b9d0b9197ff589512eb
candi-project <cchiu615@gmail.com>
2020-11-13 11:32:27 +0100

    FindElementByCss

--- SeleniumWDTutorial/FindByElement/FindElementBy.py
+++ SeleniumWDTutorial/FindByElement/FindElementBy.py
@@ -3,13 +3,16 @@ import time
 
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("/Users/candichiu/Documents/workspace_python/driver/chromedriver86")
+driver = webdriver.Chrome(executable_path="/home/candi/Desktop/Selenium/Selenium/driver/chromedriver84")
 driver.get("http://dummypoint.com/seleniumtemplate.html")
 time.sleep(3)
 
 #driver.find_element(By.ID,"user_input").send_keys("Candi Chiu")
 #driver.find_element(By.CLASS_NAME,"entertext").send_keys("Candi Chiu_ClassName")
-driver.find_element(By.NAME,"textbox").send_keys("Candi Chiu_Name")
+#driver.find_element(By.NAME,"textbox").send_keys("Candi Chiu_Name")
+#driver.find_element(By.TAG_NAME,"input").send_keys("Candi Chiu_TagName")
+#driver.find_element(By.LINK_TEXT,"Form").click()
+driver.find_element(By.PARTIAL_LINK_TEXT,"For").click()
 
 time.sleep(5)
 driver.quit()
