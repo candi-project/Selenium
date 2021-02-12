99326c59d35febeb1af7d82c2b3822198c2a5c57
candi-project <cchiu615@gmail.com>
2021-02-12 14:44:54 +0100

    Change logfile name

--- SeleniumFramework/utilities/CustomLogger.py
+++ SeleniumFramework/utilities/CustomLogger.py
@@ -1,9 +1,13 @@
 import inspect
 import logging
 import allure
+from datetime import datetime
 
 
 def customLogger():
+
+    LOG_FILENAME = datetime.now().strftime('../reports/logfile_%H_%M_%S_%d_%m_%Y.log')
+
     # 1.This is used to get the  class / method name from where this customLogger method is called.
     logName = inspect.stack()[1][3]
 
@@ -14,15 +18,16 @@ def customLogger():
     logger.setLevel(logging.DEBUG)
 
     # 4.) Create the fileHandler to save the logs in the file
-    #fileHandler = logging.FileHandler("{0}.log".format(logName), mode='a')
-    #fileHandler = logging.FileHandler(filename='Candi.log', mode='a')
-    fileHandler = logging.FileHandler("../reports/Candi.log", mode='a')
+    # fileHandler = logging.FileHandler("{0}.log".format(logName), mode='a')
+    # fileHandler = logging.FileHandler(filename='Candi.log', mode='a')
+    fileHandler = logging.FileHandler(LOG_FILENAME, mode='a')
 
     # 5.) Set the logLevel for fileHandler
     fileHandler.setLevel(logging.DEBUG)
 
     # 6.) Create the formatter in which format do you like to save the logs
-    formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s', datefmt='%d/%m/%y %H:%M:%S %p %A')
+    formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s',
+                                  datefmt='%d/%m/%y %H:%M:%S %p %A')
 
     # 7.) Set the formatter to fileHandler
     fileHandler.setFormatter(formatter)
@@ -33,7 +38,7 @@ def customLogger():
     #  9.) Finally return the logging object
     return logger
 
+
 def allureLogs(text):
     with allure.step(text):
         pass
-
